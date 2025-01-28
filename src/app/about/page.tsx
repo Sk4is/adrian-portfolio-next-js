df3f18de60d7e41deaeac98f3f61056afba28f4b
Sk4is-1 <adrianperezagredano@gmail.com>
2025-01-28 09:13:44 +0100

    changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -71,9 +71,12 @@ export default function About() {
     {
       title: about.technical.title,
       display: about.technical.display,
-      items: about.technical.skills.map((skill) => skill.title),
+      items: about.technical.skills
+        .filter((skill) => skill !== undefined)
+        .map((skill) => skill!.title),
     },
   ];
+
   return (
     <Column maxWidth="m">
       <script
@@ -356,35 +359,41 @@ export default function About() {
                 {about.technical.title}
               </Heading>
               <Column fillWidth gap="l">
-                {about.technical.skills.map((skill, index) => (
-                  <Column key={`${skill}-${index}`} fillWidth gap="4">
-                    <Text variant="heading-strong-l">{skill.title}</Text>
-                    <Text variant="body-default-m" onBackground="neutral-weak">
-                      {skill.description}
-                    </Text>
-                    {skill.images && skill.images.length > 0 && (
-                      <Flex fillWidth paddingTop="m" gap="12" wrap>
-                        {skill.images.map((image, index) => (
-                          <Flex
-                            key={index}
-                            border="neutral-medium"
-                            radius="m"
-                            minWidth={image.width}
-                            height={image.height}
-                          >
-                            <SmartImage
-                              enlarge
-                              radius="m"
-                              sizes={image.width.toString()}
-                              alt={image.alt}
-                              src={image.src}
-                            />
+                {about.technical.skills.map(
+                  (skill, index) =>
+                    skill && (
+                      <Column key={`${skill}-${index}`} fillWidth gap="4">
+                        <Text variant="heading-strong-l">{skill.title}</Text>
+                        <Text
+                          variant="body-default-m"
+                          onBackground="neutral-weak"
+                        >
+                          {skill.description}
+                        </Text>
+                        {skill.images && skill.images.length > 0 && (
+                          <Flex fillWidth paddingTop="m" gap="12" wrap>
+                            {skill.images.map((image, index) => (
+                              <Flex
+                                key={index}
+                                border="neutral-medium"
+                                radius="m"
+                                minWidth={image.width}
+                                height={image.height}
+                              >
+                                <SmartImage
+                                  enlarge
+                                  radius="m"
+                                  sizes={image.width.toString()}
+                                  alt={image.alt}
+                                  src={image.src}
+                                />
+                              </Flex>
+                            ))}
                           </Flex>
-                        ))}
-                      </Flex>
-                    )}
-                  </Column>
-                ))}
+                        )}
+                      </Column>
+                    )
+                )}
               </Column>
             </>
           )}
